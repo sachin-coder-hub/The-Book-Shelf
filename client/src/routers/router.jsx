b115d418aed1f704df51b791faf398674ced6bfe
sachin-coder-hub <sachincoder0706@gmail.com>
2025-02-16 12:42:18 +0530

    Normalize line endings

---
+++ client/src/routers/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Home page</h1>,
+      },
+    ],
+  },
+  // Add more routes as needed
+]);
+
+export default router;
